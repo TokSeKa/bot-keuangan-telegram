cf2999b9b3392034ff96af7e7283ce0bee989f65
Tok Se Ka <tokseka.id@gmail.com>
2026-07-31 02:58:52 +0700

    Update main.py
    Perbaiki letak variabel yang didalam loop while serta mengubah inisasinya agar getUpdatenya gak perlu double

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 telegram_bot_api = os.getenv("TELEGRAM_BOT_API_KEY")
 url_telegram = f"https://api.telegram.org/bot{telegram_bot_api}"
+message_to_chat = "Hai, kamu berhasil chat"
+update_id = None
+
 while True:
     # Mengirimkan request GET ke api telegram Bot dengan API yang sudah di muat update chat
-    response = requests.get(url = url_telegram+"/getUpdates", params={"timeout":300})
-    message_to_chat = "Hai, kamu berhasil chat"
-    update_id = None
-
+    if update_id:
+        response = requests.get(url = url_telegram+"/getUpdates", params={"timeout":300, "offset":update_id+1})
+    else:
+        response = requests.get(url = url_telegram+"/getUpdates", params={"timeout":300})
+        
     if response.status_code == 200: # Jika response 200 atau success
         for item in response.json().get("result"): # Untuk setiap pesan
             if "message" in item:
@@ -26,8 +30,5 @@ while True:
             
             if response_chat.status_code == 200:
                 update_id = item.get("update_id") # Ambil update_id terakhir
-            else:
-                break
-        if update_id:
-            requests.get(url = url_telegram+"/getUpdates", params={"offset":update_id+1}) # update_id confirm    
+            else: break
         
