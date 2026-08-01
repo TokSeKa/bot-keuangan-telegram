9a1389dd545fbf38a58e405de5841c9a9a854986
Tok Se Ka <tokseka.id@gmail.com>
2026-08-01 16:31:09 +0700

    Alur insert berhasil
    - Ketika user menginsert kan item, maka akan tercatat di case normal
    - RETURNING pada DB dipakai sebagai validator apakah sudah masuk atau belum
    - Modul sudah terpecah agar modular
    - Gemini sudah meresponse dengan struktur yang rigid
    - Database berhasil menerima parameter dan melakukan insert

---
+++ gemini_conn.py
@@ -0,0 +1,49 @@
+from google import genai
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from typing import Union, Literal
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+gemini_api = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=gemini_api)
+
+pilihan_model = [ #hanya untuk testing
+    # --- Gemini 3 Series ---
+    "gemini-3.6-flash",
+    "gemini-3.5-flash",
+    "gemini-3.5-flash-lite",
+    "gemini-3.1-pro",
+    "gemini-3.1-flash-lite",
+    "gemini-3-flash",
+
+    # --- Gemini 2.5 Series ---
+    "gemini-2.5-pro",
+    "gemini-2.5-flash",
+    "gemini-2.5-flash-lite",
+]
+
+# Structured outpu
+class Transaksi(BaseModel):
+    nama: str = Field(description="Nama aktifitas atau objek transaksi")
+    nominal: int = Field(description="Jumlah nominal akitifitas transaksi, Sesuaikan dengan konteks aktifitas, kurs rupiah biasanya memakai ribuan rupiah hingga jutaan rupiah, sesuaikan dengan value dari aktifitas yang dimaksud, contoh 34 dalam konteks makanan kemungkinan besaar 34000 .Kurs dalam Rupiah, jika secara spesifik memakai kurs asing maka konversikan secara perkiraan.")
+    catatan: Optional[str] = Field(description="Catatan kaki dari user.")
+    kategori: Literal["Lainnya", "Makanan", "Minuman", "Pakaian", "Alat mandi", "Tagihan Rumah", "Transportasi", "Telepon", "Sosial", "Perbaikan", "Kesehatan", "Olahraga", "Hiburan", "Pendidikan"] = Field(description="Kategori aktivitas yang disebut user")
+    tipe: Literal["Pengeluaran", "Pemasukan"] = Field(description="Tipe aktifitas berupa ingin melakukan transaksi Pengeluaran atau Pemasukan")
+    fungsi: Literal["Pencatatan", "Perubahan", "Penghapusan", "Pencarian"] = Field(description="Jenis fungsi yang perlu dipakai dalam aktifitas ini.")
+    response: str = Field(description="Respon Terhadap permintaan user yang dipakai untuk menjawab chat user")
+
+# Fungsi mengirimkan ke gemini, nantinya text nya di balut dengan promt
+def send_chat_llm(input_user, model="gemini-3.5-flash"):
+    interaction  = client.interactions.create(
+            model=model,
+            input=input_user,
+            response_format={
+                "type": "text",
+                "mime_type": "application/json",
+                "schema": Transaksi.model_json_schema()
+            },
+        )
+    return Transaksi.model_validate_json(interaction.output_text)
--- main.py
+++ main.py
@@ -1,43 +1,69 @@
+# IMPORT PYTHON
 import requests
 import os
 from dotenv import load_dotenv
-from google import genai
+from datetime import datetime, timezone
+
+#Own function
+from database import insert_transaksi
+from gemini_conn import send_chat_llm
 
 # Memuat seluruh variabel .env 
 load_dotenv()
 telegram_bot_api = os.getenv("TELEGRAM_BOT_API_KEY")
 gemini_api = os.getenv("GEMINI_API_KEY")
 url_telegram = f"https://api.telegram.org/bot{telegram_bot_api}"
+
+# Fungsi global
 update_id = None
 
-client = genai.Client(api_key=gemini_api)
+# Main run
 while True:
     # Mengirimkan request GET ke api telegram Bot dengan API yang sudah di muat update chat
     if update_id:
         response = requests.get(url = url_telegram+"/getUpdates", params={"timeout":300, "offset":update_id+1})
     else:
         response = requests.get(url = url_telegram+"/getUpdates", params={"timeout":300})
-        
-    if response.status_code == 200: # Jika response 200 atau success
-        for item in response.json().get("result"): # Untuk setiap pesan
-            if "message" in item:
-                type_chat = item.get("message")
-            elif "edited_message" in item:
-                type_chat = item.get("edited_message")
-            else:
-                continue # sementara belum tahu
-            
-            chat_id = type_chat.get('chat').get("id") # Ambil id chat user
-            text_user = type_chat.get('text', "User tidak mengirimkan text")
-
-            interaction = client.interactions.create(
-                model="gemini-3.6-flash",
-                input=text_user
-            )
-            response_gemini = interaction.output_text
-            response_chat = requests.get(url = url_telegram+'/sendMessage', params={"chat_id": chat_id, "text":response_gemini}) # kirimkan pesan kepada user     
-            print(response_chat.status_code)
-            if response_chat.status_code == 200:
-                update_id = item.get("update_id") # Ambil update_id terakhir
-            else: break
+    
+    print (response.json())
+    match response.status_code:
+        case 200:
+            for item in response.json().get("result"): # Untuk setiap pesan
+                tipe_konteks_chat = "Ini adalah " 
+                tanggal_edit = None
+                # Check apa tipe pesannya:
+                if "message" in item:
+                    type_chat = "message"
+                    tanggal_input = datetime.fromtimestamp(item.get(type_chat).get('date'), tz=timezone.utc)
+                    tipe_konteks_chat = tipe_konteks_chat + f"Pesan baru dari User dengan tanggal input {tanggal_input}"
+                elif "edited_message" in item: # Rencana: Jika edit maka pindahkan ke konteks edit yang pernah ada
+                    type_chat = "edited_message"
+                    tanggal_input = datetime.fromtimestamp(item.get(type_chat).get('date'), tz=timezone.utc)
+                    tanggal_edit = datetime.fromtimestamp(item.get(type_chat).get('edit_date'), tz=timezone.utc)
+                    tipe_konteks_chat = tipe_konteks_chat + f"Pesan lama yang di edit User dengan tanggal input {tanggal_input}, dan tanggal edit {tanggal_edit} "
+                else:
+                    continue # sementara belum tahu
+                
+                chat_id = item.get(type_chat).get('chat').get("id") # Ambil id chat user
+                text_user = tipe_konteks_chat+"\nBerikut pesan user: "+ item.get(type_chat).get('text', "User tidak mengirimkan text") #Bisa aja kirim gambar doang
+                
+                response_gemini = send_chat_llm(input_user=text_user) # Mengirim ke Gemini
+                
+                # Insert transaksi
+                if (response_gemini.fungsi == "Pencatatan"):   
+                    response_insert = insert_transaksi(nama=response_gemini.nama, 
+                                           nominal=response_gemini.nominal, 
+                                           catatan=response_gemini.catatan, 
+                                           kategori=response_gemini.kategori,
+                                           tanggal=tanggal_input, 
+                                           tipe=response_gemini.tipe, 
+                                           chat_id=chat_id)
+                    print(response_insert)
+                    response_insert_telegram = f"{response_insert[1]} dengan nominal Rp.{response_insert[2]} telah tercatat!\nKategori: {response_insert[4]}\nWaktu: {response_insert[5].strftime("%d %B %Y, %H:%M WIB")} \nTipe: {response_insert[6]}"
+                
+                response_chat = requests.get(url = url_telegram+'/sendMessage', params={"chat_id": chat_id, "text":response_insert_telegram}) # kirimkan pesan kepada user     
+                
+                if response_chat.status_code == 200:
+                    update_id = item.get("update_id") # Ambil update_id terakhir
+                else: break
         
